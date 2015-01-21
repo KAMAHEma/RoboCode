a502f789118ce0a267e025b91deb7cfd935f0b6e
Ren Nagasaki <Ren Nagasaki@192.168.214.79>
2015-01-21 08:31:28 +0100

    1.0

--- Robo Code/Robot1.java
+++ Robo Code/Robot1.java
@@ -1,7 +1,19 @@
 package Selfmade;
+import robocode.*;
 import robocode.Robot;
 
 
 public class Robot1 extends Robot {
+	
+	public void run() {
+		turnGunLeft(360);
+		turnGunRight(360);
+		while(true) {
+			
+		}
+	}
 
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
 }
