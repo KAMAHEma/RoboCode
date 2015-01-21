5e0fa744951f3dc9e80823d865ed8930e07e508e
danielm <danielm@192.168.214.63>
2015-01-21 12:03:34 +0100

    daniel 1.101

--- RoboCode/src/Selfmade_Robots/RobotDaniel.java
+++ RoboCode/src/Selfmade_Robots/RobotDaniel.java
@@ -1,19 +1,83 @@
 package Selfmade_Robots;
 
+import java.awt.Color;
+import java.util.Random;
+
 import robocode.AdvancedRobot;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
+import robocode.TurnCompleteCondition;
 
 public class RobotDaniel extends AdvancedRobot {
+	private boolean isMovingForward;
+
 	public void run() {
-        while (true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-            System.out.println();
-        }
-    }
- 
-    public void onScannedRobot(ScannedRobotEvent event) {
-    }
+		setBodyColor(new Color(0, 0, 0));
+		setGunColor(new Color(0, 240, 56));
+		setRadarColor(new Color(179, 9, 32));
+		setBulletColor(new Color(255, 102, 0));
+		setScanColor(new Color(179, 9, 32));
+		while (true) {
+			setAhead(getRandomDistance());
+			isMovingForward = true;
+			setTurnRight(90);
+			// Wir drehen uns einmal zu Ende
+			waitFor(new TurnCompleteCondition(this));
+			// nun wechseln wir unsere Kurvenrichtung
+			setTurnLeft(180);
+			waitFor(new TurnCompleteCondition(this));
+			// nun wechseln wir unsere Kurvenrichtung
+			setTurnRight(180);
+			waitFor(new TurnCompleteCondition(this));
+		}
+	}
+
+	public int getRandomDistance() {
+		Random rand = new Random();
+		int n = rand.nextInt(40000) + 25000;
+		return n;
+	}
+
+	public void onHitWall(HitWallEvent e) {
+		fireTurnAround();
+	}
+
+	public void fireTurnAround() {
+		if (isMovingForward) {
+			setBack(getRandomDistance());
+			isMovingForward = false;
+		} else {
+			setAhead(getRandomDistance());
+			isMovingForward = true;
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		int power4distance = renderFireDistance(e.getDistance());
+		if (power4distance != -1) {
+			long shotTravelTime = (long) (e.getDistance() / (20 - power4distance * 3));
+			
+			fire(power4distance);
+		}
+		
+	}
+
+	public int renderFireDistance(double robotDistance) {
+		int power4distance = -1;
+		if (robotDistance > 200 || getEnergy() < 15) {
+			power4distance = 1;
+		} else if (robotDistance > 50) {
+			power4distance = 2;
+		} else {
+			power4distance = 3;
+		}
+		return power4distance;
+	}
+
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.isMyFault()) {
+			fireTurnAround();
+		}
+	}
 }
