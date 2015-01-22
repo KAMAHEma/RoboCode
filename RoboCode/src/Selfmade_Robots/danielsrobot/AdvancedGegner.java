76cefaaa62aa79ecfc74e988a09ff007a920c5b3
danielm <danielm@192.168.214.63>
2015-01-22 08:08:25 +0100

    zusatz

---
+++ RoboCode/src/Selfmade_Robots/danielsrobot/AdvancedGegner.java
@@ -0,0 +1,48 @@
+package Selfmade_Robots.danielsrobot;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class AdvancedGegner extends Gegner {
+
+	public AdvancedGegner() {
+		reset();
+	}
+
+	private double xCord;
+	private double yCord;
+
+	public double getxCord() {
+		return xCord;
+	}
+	public double getyCord() {
+		return yCord;
+	}
+
+	@Override
+	public void reset() {
+		xCord = 0;
+		yCord = 0;
+		super.reset();
+	}
+
+	public void update(ScannedRobotEvent event, Robot robot) {
+		super.update(event);
+
+		double absBearingDeg = (robot.getHeading() + event.getBearing());
+		if (absBearingDeg < 0) {
+			absBearingDeg += 360;
+		}
+
+		xCord = robot.getX() + Math.sin(Math.toRadians(absBearingDeg)) * event.getDistance();
+		yCord = robot.getY() + Math.cos(Math.toRadians(absBearingDeg)) * event.getDistance();
+	}
+
+	public double getPredictedX(long bulletTravelTime) {
+		return xCord + Math.sin(Math.toRadians(getHeading())) * getVelocity() * bulletTravelTime;
+	}	
+
+	public double getPredictedY(long bulletTravelTime) {
+		return yCord + Math.sin(Math.toRadians(getHeading())) * getVelocity() * bulletTravelTime;
+	}
+}
---
+++ RoboCode/src/Selfmade_Robots/danielsrobot/Gegner.java
@@ -0,0 +1,68 @@
+package Selfmade_Robots.danielsrobot;
+import robocode.ScannedRobotEvent;
+
+public class Gegner {
+
+	/**
+	 * GegernObject welches representativ für den gescannten Gegner steht
+	 */
+	public Gegner() {
+		// noch machen wir hir nichts, das knn sich aber ändern
+		reset();
+	}
+
+	private double bearing;
+	private double distance;
+	private double energy;
+	private double heading;
+	private double velocity;
+	private String name;
+
+	public double getBearing() {
+		return bearing;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public double getEnergy() {
+		return energy;
+	}
+	public double getHeading() {
+		return heading;
+	}
+	public double getVelocity() {
+		return velocity;
+	}
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * aktualisiert den gescannten Gegner mit den vorhanden Daten
+	 * @param event
+	 */
+	public void update(ScannedRobotEvent event) {
+		bearing = event.getBearing();
+		distance = event.getDistance();
+		heading = event.getHeading();
+		energy = event.getEnergy();
+		velocity = event.getVelocity();
+		name = event.getName();
+	}
+
+	/**
+	 * wir clearen alle GegnerDaten für später Zwecke
+	 */
+	public void reset() {
+		bearing = 0.0;
+		distance = 0.0;
+		heading = 0.0;
+		energy = 0.0;
+		velocity = 0.0;
+		name = "";
+	}
+
+	public boolean isEnemyKnown() {
+		return !"".equals(name);
+	}
+}
