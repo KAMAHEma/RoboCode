a2b60cae8f26624554dc41ac5a6ff4a3382a108d
Ren Nagasaki <Ren Nagasaki@192.168.214.79>
2015-01-22 08:07:55 +0100

    1.2

--- RoboCode/src/Selfmade_Robots/RobotJulian.java
+++ RoboCode/src/Selfmade_Robots/RobotJulian.java
@@ -1,13 +1,15 @@
 package Selfmade_Robots;
 import robocode.*;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
+
 import java.awt.*;
 
 public class RobotJulian extends AdvancedRobot {
 	boolean movingForward; // Is set to true when setAhead is called, set to false on setBack
+	int speed;
 	boolean inWall; // Is true when robot is near the wall.
-	int dist = 50; // distance to move when we're hit
-	
+	int i = 0;
+	int dist = 50; // distance to move when we're hit	
 
 	/**
 	 * run: j Fire's main run function
@@ -24,12 +26,22 @@ public class RobotJulian extends AdvancedRobot {
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 		
+		movingForward = true;
+		
 		while(true) {
-			turnGunRight(10);
-			turnRadarRight(10);			
+			turnGunRight(30);
+			turnRadarRight(30);
 		}
 	}
 
+	public void move(HitByBulletEvent e) {
+		turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
+
+		ahead(dist);
+		dist *= -1;
+		scan();
+	}
+	
 	/**
 	 * onScannedRobot:  Fire!
 	 */
@@ -37,13 +49,16 @@ public class RobotJulian extends AdvancedRobot {
 		double absoluteBearing = getHeading() + e.getBearing();
 		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 		double bearingFromRadar = normalRelativeAngleDegrees(absoluteBearing - getRadarHeading());
- 
+		double firePower = 0;
+		double distance = e.getDistance();
+		
 		if (Math.abs(bearingFromGun) <= 4) {
 			setTurnGunRight(bearingFromGun); 
 			setTurnRadarRight(bearingFromRadar);
 			
 			if (getGunHeat() == 0 && getEnergy() > .2) {
-				fire(Math.min(4.5 - Math.abs(bearingFromGun) / 2 - e.getDistance() / 250, getEnergy() - .1));
+				firePower = Math.min(4.5 - Math.abs(bearingFromGun) / 2 - e.getDistance() / 250, getEnergy() - .1);
+				fire(firePower);
 			} 
 		}
 		else {
@@ -53,15 +68,13 @@ public class RobotJulian extends AdvancedRobot {
 		if (bearingFromGun == 0) {
 			scan();
 		}
+		speed = ((int) distance) / ((int) ((20 - firePower)*3));
+		
 		execute();
 	}
 	
-	public void onHitWall(HitWallEvent e) {
+	public void onHitByBullet(HitByBulletEvent e) {
+		move(e);
 	}
-	
-	/**
-	 * onHitRobot:  Aim at it.  Fire Hard!
-	 */
-	public void onHitRobot(HitRobotEvent e) {
-	}
+
 }
